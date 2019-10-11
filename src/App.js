07884d24e7d92c2d035060b5383de5ce2406699e
Eduardo Freitas <edusf8700@gmail.com>
2019-10-10 22:26:11 -0300

    Criado a tela de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Edu</h1>
-    </div>
+   <div className="container">
+     <img src={logo} alt="AirCnc"/>
+
+     <div className="content">
+       <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>taletos</strong> para sua empresa</p>
+
+       <form >
+         <label htmlFor="email">E-MAIL *</label>
+         <input 
+          type="email" 
+          id="email" 
+          placeholder="Seu melhor e-mail"/>
+          <button type="submit" className= "btn">Entrar</button>
+       </form>
+
+     </div>
+
+   </div>
   );
 }
 

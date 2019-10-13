78825afee5b9b8da7c1aaff8a8501f3991c519c2
Eduardo Freitas <edusf8700@gmail.com>
2019-10-13 02:21:08 -0300

    armazenado id do usuário no localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
+
 import logo from './assets/logo.svg';
 
 function App() {
+  const [ email, setEmail ] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+
+
+  }
+
   return (
    <div className="container">
      <img src={logo} alt="AirCnc"/>
@@ -10,12 +26,16 @@ function App() {
      <div className="content">
        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>taletos</strong> para sua empresa</p>
 
-       <form >
+       <form onSubmit={handleSubmit}>
          <label htmlFor="email">E-MAIL *</label>
          <input 
           type="email" 
           id="email" 
-          placeholder="Seu melhor e-mail"/>
+          value={email}
+          placeholder="Seu melhor e-mail"
+          onChange={event => setEmail(event.target.value)} 
+         />
+
           <button type="submit" className= "btn">Entrar</button>
        </form>
 

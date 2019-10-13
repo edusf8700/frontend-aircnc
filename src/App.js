03ee8992dce70e055dfc001be27bb1572a9a63a8
Eduardo Freitas <edusf8700@gmail.com>
2019-10-13 03:17:05 -0300

    Criado rotas

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
+import Routes from './routes';
 
 import logo from './assets/logo.svg';
 
 function App() {
-  const [ email, setEmail ] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault();
-
-    const response = await api.post('/sessions', { email });
-
-    const { _id } = response.data;
-
-    localStorage.setItem('user', _id);
-
-
-  }
-
+ 
   return (
    <div className="container">
      <img src={logo} alt="AirCnc"/>
 
      <div className="content">
-       <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>taletos</strong> para sua empresa</p>
-
-       <form onSubmit={handleSubmit}>
-         <label htmlFor="email">E-MAIL *</label>
-         <input 
-          type="email" 
-          id="email" 
-          value={email}
-          placeholder="Seu melhor e-mail"
-          onChange={event => setEmail(event.target.value)} 
-         />
-
-          <button type="submit" className= "btn">Entrar</button>
-       </form>
+       <Routes />
 
      </div>
 
